bd1f664c69b0af9363d5492dc2ef651a857f39ee
Eva-Maria <evamaria.asplund@gmail.com>
2024-09-14 21:21:23 +0300

    public key stuff

--- src/salaus.py
+++ src/salaus.py
@@ -1,4 +1,33 @@
+import random
+import math
+
 class Salaus:
     """Luokka vastaa viestin salaamisesta ja salauksen purkamisesta"""
 
-    pass
+    def __init__(self):
+        """p ja q jotka muodostavat julkisen avaimen n osuuden"""
+        
+        self.p = None
+        self.q = None
+
+    def generate_prime(self):
+        """Satunnaisten, toisistaan riippumattomien alkulukujen generoimiseen"""
+
+        pass
+
+    def public_key_n(self):
+        """Julkisen avaimen n osan laskemiseen"""
+
+        p = self.generate_prime()
+        self.p = 12
+        q = self.generate_prime()
+        self.q = 13
+        n = p * q
+        return n
+
+    def public_key_e(self):
+        """Julkisen avaimen e osan laskemiseen"""
+
+        e_smaller_than = (self.p-1)*(self.q-1)
+        e = random.randint(2, e_smaller_than-1)
+        return e
