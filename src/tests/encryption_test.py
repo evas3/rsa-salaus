2712180c7ed4e4ea88bece66c3a88e2b87782d4a
Eva-Maria <evamaria.asplund@gmail.com>
2024-10-23 15:01:46 +0300

    editing tests

--- src/tests/encryption_test.py
+++ src/tests/encryption_test.py
@@ -7,35 +7,49 @@ class TestEncryption(unittest.TestCase):
         pass
 
     def test_message_to_int_txt_input(self):
-        message = "Hello World1!"
+        message = "Hello This Is ä message to you all!-*/32312"
         encoded = Encryption().message_to_int(message)
         decoded = Decryption().message_to_str(encoded)
         self.assertEqual(decoded, message)
 
     def test_message_to_int_int_input(self):
-        message = "34545"
+        message = "34748735453846387366385743658376475365"
+        encoded = Encryption().message_to_int(message)
+        decoded = Decryption().message_to_str(encoded)
+        self.assertEqual(decoded, message)
+
+    def test_message_to_int_empty_input(self):
+        message = ""
         encoded = Encryption().message_to_int(message)
         decoded = Decryption().message_to_str(encoded)
         self.assertEqual(decoded, message)
 
     def test_message_to_int_is_int(self):
-        message = "Hello World!"
+        message = "Hello This Is ä message to you all!-*/32312"
         encoded = Encryption().message_to_int(message)
         self.assertEqual(encoded.is_integer(), True)
 
     def test_encryption_is_integer(self):
-        message = 3474873545
+        message = 34748735453846387366385743658376475365
         encrypted = Encryption().encryption(message, 65537)
         self.assertEqual(encrypted[0].is_integer(), True)
 
     def test_encryption_num(self):
-        message = 3474873545
+        message = 34748735453846387366385743658376475365
         encrypted_info = Encryption().encryption(message, 65537)
         decrypted = Decryption().decryption(encrypted_info[0], encrypted_info[2], encrypted_info[1])
         self.assertEqual(message, decrypted)
 
     def test_encryption_txt(self):
-        message = "Hello"
+        message = "Hello This Is ä message to you all!-*/32312"
+        encoded = Encryption().message_to_int(message)
+        encrypted_info = Encryption().encryption(encoded, 65537)
+        decrypted = Decryption().decryption(encrypted_info[0], encrypted_info[2], encrypted_info[1])
+        decoded = Decryption().message_to_str(decrypted)
+        self.assertEqual(message, decoded)
+
+    def test_encryption_empty(self):
+        message = ""
         encoded = Encryption().message_to_int(message)
         encrypted_info = Encryption().encryption(encoded, 65537)
         decrypted = Decryption().decryption(encrypted_info[0], encrypted_info[2], encrypted_info[1])
